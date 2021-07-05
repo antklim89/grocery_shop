fb8237999cef9194f1feb13ce270638b15264cfa
antklim89 <antklim89@mail.ru>
2021-07-05 18:43:05 +0000

    update

--- components/about/Features.tsx
+++ components/about/Features.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import ReactMarkdown from 'react-markdown';
 
 import { IFeature } from '~/types';
 
@@ -8,10 +9,10 @@ export default function Features({ features }: {features: IFeature[]}): JSX.Elem
         <section className="mb-5 py-2 bg-dark text-light">
             <div className="container">
                 <h1 className="text-center">Features</h1>
-                <div className="row gx-2">
+                <div className="row g-2">
                     {features.map((feature) => (
-                        <div className="col-xl-3 col-lg-6 col-sm-12" key={feature.id}>
-                            <section className="card  bg-light text-dark">
+                        <div className="col-xl-3 col-md-6 col-xs-12 align-items-stretch" key={feature.id}>
+                            <section className="card  bg-light text-dark h-100">
                                 <Image
                                     alt={feature.title}
                                     className="card-img-top"
@@ -22,7 +23,7 @@ export default function Features({ features }: {features: IFeature[]}): JSX.Elem
                                 />
                                 <div className="card-body">
                                     <h3 className="card-title text-center text-uppercase">{feature.title}</h3>
-                                    <p className="card-text">{feature.feature}</p>
+                                    <ReactMarkdown className="card-text">{feature.feature}</ReactMarkdown>
                                 </div>
                             </section>
                         </div>
