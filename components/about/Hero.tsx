b2dd7632c69b1dc4efcd0c5f4567da77f420bcec
antklim89 <antklim89@mail.ru>
2021-07-13 00:16:25 +0000

    update Hero

--- components/about/Hero.tsx
+++ components/about/Hero.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import style from '~/styles/Hero.module.scss';
 import { HeroProps } from '~/types';
 
 
@@ -18,12 +19,12 @@ export default function Hero({ image, title, text }: HeroProps): JSX.Element {
                 src={image.url}
                 width={1280}
             />
-            <div className="mx-auto position-absolute top-50 translate-middle-y w-100">
+            <div className={`${style.text} p-2 mx-auto position-absolute top-50 translate-middle-y w-100`}>
                 <div className="w-50 mx-auto">
-                    <h1 className="text-center fs-1 text-light">{title}</h1>
-                    <p className="text-center d-none d-lg-block fs-1 text-light">{text}</p>
+                    <h1 className="text-center fs-1">{title}</h1>
+                    <p className="text-center d-none d-lg-block fs-1">{text}</p>
                     <Link passHref href="/products">
-                        <button className="btn btn-outline-light btn-lg d-block mx-auto" type="button">
+                        <button className="btn btn-outline-primary btn-lg d-block mx-auto" type="button">
                             Get Catalog
                         </button>
                     </Link>
