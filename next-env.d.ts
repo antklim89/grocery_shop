f81c09e0c4ab113dadb3410ab42026e27f809ea6
Anton <antklim89@mail.ru>
2021-08-28 13:42:38 +0300

    update

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,5 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable multiline-comment-style */
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
