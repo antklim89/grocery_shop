78977152faec03a9e1f7bbad84257034a5c50df1
Anton <31469798+antklim89@users.noreply.github.com>
2021-09-11 14:28:43 +0000

    debug

--- components/utils/StrapiImage.tsx
+++ components/utils/StrapiImage.tsx
@@ -11,10 +11,10 @@ type Props = Omit<ImageProps, 'src'|'width'|'height'| 'blurDataURL'> & {
     height: number
 }
 
-function getImage(type: StripiFormatType, src: string, image: IStrapiImage): string {
-    const imageFormat = image.formats[type];
-    return `${src}${imageFormat ? imageFormat.url : image.url}`;
-}
+// function getImage(type: StripiFormatType, src: string, image: IStrapiImage): string {
+//     const imageFormat = image.formats[type];
+//     return `${src}${imageFormat ? imageFormat.url : image.url}`;
+// }
 
 const StrapiImage: FC<Props> = ({
     image,
@@ -24,39 +24,42 @@ const StrapiImage: FC<Props> = ({
     ...props
 }) => {
 
-    const loader = useCallback(({ src, width: loaderWidth }: {src: string, width: number}) => {
-        switch (loaderWidth) {
-        case 640:
-            return getImage('thumbnail', src, image);
-        case 750: case 828: case 1080:
-            if (width < 320) return getImage('thumbnail', src, image);
-            return getImage('small', src, image);
-        case 1200: case 1920:
-            if (width < 320) return getImage('thumbnail', src, image);
-            if (width < 640) return getImage('small', src, image);
-            return getImage('medium', src, image);
-        case 2048: case 3840:
-            if (width < 320) return getImage('thumbnail', src, image);
-            if (width < 640) return getImage('small', src, image);
-            if (width < 1080) return getImage('medium', src, image);
-            return getImage('large', src, image);
-        default:
-            return `${src}${image.url}`;
-        }
-    }, [image.url]);
+    // const loader = useCallback(({ src, width: loaderWidth }: {src: string, width: number}) => {
+    //     switch (loaderWidth) {
+    //     case 640:
+    //         return getImage('thumbnail', src, image);
+    //     case 750: case 828: case 1080:
+    //         if (width < 320) return getImage('thumbnail', src, image);
+    //         return getImage('small', src, image);
+    //     case 1200: case 1920:
+    //         if (width < 320) return getImage('thumbnail', src, image);
+    //         if (width < 640) return getImage('small', src, image);
+    //         return getImage('medium', src, image);
+    //     case 2048: case 3840:
+    //         if (width < 320) return getImage('thumbnail', src, image);
+    //         if (width < 640) return getImage('small', src, image);
+    //         if (width < 1080) return getImage('medium', src, image);
+    //         return getImage('large', src, image);
+    //     default:
+    //         return `${src}${image.url}`;
+    //     }
+    // }, [image.url]);
 
 
-    const blurDataURL = image.url.startsWith('http') ? image.formats.thumbnail.url : `${API_URL}${image.formats.thumbnail.url}`;
-    const src = image.url.startsWith('http') ? '' : API_URL;
+    // eslint-disable-next-line no-console
+    console.debug('image: \n', JSON.stringify(image, null, 4));
+
+    // const blurDataURL = image.url.startsWith('http') ? image.formats.thumbnail.url : `${API_URL}${image.formats.thumbnail.url}`;
+    // const src = image.url.startsWith('http') ? '' : API_URL;
 
     return (
         <Image
             alt={alt}
             layout="responsive"
             placeholder="blur"
-            src={{ src, blurDataURL, width, height }}
+            src={{ src: 'https://ucarecdn.com/37b53d5b-a126-4c6f-abe0-2a2fa3b21fec/', blurDataURL: 'https://ucarecdn.com/37b53d5b-a126-4c6f-abe0-2a2fa3b21fec/', width, height }}
             {...props}
-            loader={loader}
+            // loader={loader}
         />
     );
 };
