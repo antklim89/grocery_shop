f1651822176f468fa8fa19101ce71d0ce80747ab
Anton <antklim89@mail.ru>
2022-01-16 14:52:03 +0300

    updated Hero

--- components/about/Hero.tsx
+++ components/about/Hero.tsx
@@ -9,18 +9,20 @@ import { HeroProps } from '~/types';
 const Hero = ({ image, title, text }: HeroProps): JSX.Element => {
     return (
         <section className={style.section}>
-            <StrapiImage
-                alt="hero"
-                className="img-fluid d-block mx-auto"
-                height={500}
-                image={image}
-                objectFit="cover"
-                width={1280}
-            />
-            <div className={style.text}>
+            <div className={`${style.image} position-absolute w-100 h-100`}>
+                <StrapiImage
+                    alt="hero"
+                    className="d-block mx-auto absolute"
+                    height={720}
+                    image={image}
+                    objectFit="cover"
+                    width={1280}
+                />
+            </div>
+            <div className={`${style.text} container`}>
                 <div className="m-auto">
                     <h1 className="text-center fs-1">{title}</h1>
-                    <p className="text-center d-none d-lg-block fs-1">{text}</p>
+                    <p className="text-center d-none d-lg-block fs-4">{text}</p>
                     <Link passHref href="/product">
                         <button className="btn btn-outline-primary btn-lg d-block mx-auto" type="button">
                             Show Products
