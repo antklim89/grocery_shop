10a7406af156d0edce2efc675dc20f96b10e05db
Anton <antklim89@mail.ru>
2024-10-28 19:35:47 +0300

    feat: add client and server pocketbase

---
+++ client/lib/pocketbase/server.ts
@@ -0,0 +1,22 @@
+import { cookies } from 'next/headers';
+import PocketBase from 'pocketbase';
+
+
+if (typeof window !== 'undefined') throw new Error('This file should be imported on the server only');
+
+export async function initPocketBase() {
+  const pb = new PocketBase(process.env.SERVER_URL);
+
+  const authCookie = cookies().get('pb_auth');
+
+  if (!authCookie) return pb;
+
+  pb.authStore.loadFromCookie(`${authCookie.name}=${authCookie.value}`);
+  try {
+    pb.authStore.isValid && await pb.collection('users').authRefresh();
+  } catch {
+    pb.authStore.clear();
+  }
+
+  return pb;
+}
