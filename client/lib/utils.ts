a49c8b43ff546a70042d08085e28ea34f83abf2f
Anton <antklim89@mail.ru>
2024-11-06 21:01:08 +0300

    feat(lib): add price utils

--- client/lib/utils.ts
+++ client/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const priceFormat = Intl.NumberFormat('en-Us', {
+  maximumFractionDigits: 2,
+  minimumFractionDigits: 2,
+  currency: 'USD',
+  style: 'currency',
+  currencyDisplay: 'symbol',
+  useGrouping: false,
+});
+
+export function getPrice(args: { price: number; discount?: number; qty?: number }): string {
+  return priceFormat.format(calculatePrice(args));
+}
+
+export function calculatePrice({
+  price = 0,
+  discount = 0,
+  qty = 1,
+}: { price: number; discount?: number; qty?: number }): number {
+  return (price - price * (discount / 100)) * qty;
 }
