e5b519a1b96e8ee03872c75e059b4a964aedf375
Anton <antklim89@mail.ru>
2024-11-06 21:23:35 +0300

    feat: add h2

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,6 +7,7 @@ async function HomePage() {
     <div>
       <Hero />
       <section className="container my-8">
+        <h2 className="text-4xl font-bold mb-4 ml-8">New Products</h2>
         <ProductList perPage={3} />
       </section>
     </div>
