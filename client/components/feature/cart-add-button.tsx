7ef1da334d1d3a922d16f97f442e581117ef572b
Anton <antklim89@mail.ru>
2024-11-16 16:32:02 +0300

    feat(components): add cart add button

---
+++ client/components/feature/cart-add-button.tsx
@@ -0,0 +1,86 @@
+'use client';
+import type { CartItem, ProductType } from '@/lib/types';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  useAddCart,
+  useGetCart,
+  useRemoveCart,
+  useUpdateCart,
+} from '@/lib/queries/cart';
+import { ShoppingCart } from 'lucide-react';
+import { type FormEvent, useEffect, useState } from 'react';
+
+
+function CartAddButton({ product }: { product: ProductType }) {
+  const { data: cart, isLoading } = useGetCart();
+  const cartItem = cart?.find(item => item.product.id === product.id);
+
+  if (isLoading) return <CartAddButtonSkeleton />;
+  return <CartAddButtonForm cartItem={cartItem} product={product} />;
+}
+
+function CartAddButtonForm({ cartItem, product }: { cartItem?: CartItem; product: ProductType }) {
+  const {
+    id,
+    unit,
+    name,
+    price,
+  } = product;
+  const [qty, setQty] = useState(() => cartItem?.qty ?? 1);
+
+  const { trigger: addCart } = useAddCart();
+  const { trigger: removeCart } = useRemoveCart();
+  const { trigger: updateCart } = useUpdateCart();
+
+  useEffect(() => {
+    if (cartItem == null) return;
+    void updateCart({ cartProductId: id, cartItemUpdate: { qty } });
+  }, [qty, id, updateCart, cartItem]);
+
+  async function handleAddToCart(e: FormEvent<HTMLFormElement>): Promise<void> {
+    e.preventDefault();
+    if (cartItem) await removeCart({ cartProductId: id });
+    else await addCart({ cartItem: { product: { id, name, price }, qty } });
+  }
+
+  return (
+    <form
+      className="flex flex-col-reverse sm:flex-row mt-6 gap-2 sm:gap-4 sm:items-center sm:mt-8"
+      onSubmit={handleAddToCart}
+    >
+      <Button className="space-x-2">
+        <ShoppingCart />
+        <span>{cartItem == null ? 'Add to cart' : 'Remove from cart'}</span>
+      </Button>
+
+      <div className="flex items-center gap-2">
+        <Input
+          required
+          className="min-w-48"
+          max={1000000}
+          min={1}
+          name="qte"
+          placeholder="Quantity"
+          type="number"
+          value={qty}
+          onBlur={e => setQty(Number(e.target.value))}
+          onChange={e => setQty(Number.parseInt(e.target.value, 10))}
+        />
+        {unit}
+      </div>
+    </form>
+  );
+}
+
+function CartAddButtonSkeleton() {
+  return (
+    <div className="flex flex-col-reverse sm:flex-row mt-6 gap-2 sm:gap-4 sm:items-center sm:mt-8">
+      <Skeleton className="w-[150px] h-10" />
+      <Skeleton className="w-[150px] h-10" />
+    </div>
+  );
+}
+
+export { CartAddButton };
