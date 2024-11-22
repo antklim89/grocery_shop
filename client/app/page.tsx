5b479e8e619d96bf6cc32c968d446c62745ab148
Anton <=>
2024-11-22 19:12:39 +0300

    feat: add products with discount

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -6,9 +6,12 @@ async function HomePage() {
   return (
     <div>
       <Hero />
-      <section className="container my-8">
+      <section className="container space-y-4 md:space-y-12 my-4 md:my-8">
         <h2 className="text-4xl font-bold mb-4 ml-8">New Products</h2>
-        <ProductList perPage={3} />
+        <ProductList perPage={6} sort="-created,id" />
+
+        <h2 className="text-4xl font-bold mb-4 ml-8">Best Discount</h2>
+        <ProductList perPage={6} sort="-discount" />
       </section>
     </div>
   );
