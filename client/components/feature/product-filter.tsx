b7869f1c44787bd82b53cee606ced934037d15d4
Anton <antklim89@mail.ru>
2024-11-27 19:33:12 +0300

    feat(components): update ProductFilter

--- client/components/feature/product-filter.tsx
+++ client/components/feature/product-filter.tsx
@@ -1,16 +1,42 @@
+'use client';
 import { Input } from '@/components/ui/input';
+import { useSearchParamsState } from '@/hooks/use-searchparams-state';
 
 
 export default function ProductFilter() {
+  const [name, setName] = useSearchParamsState('name');
+  const [country, setCountry] = useSearchParamsState('country');
+  const [category, setCategory] = useSearchParamsState('category');
+
   return (
     <aside>
       <label>
         Name:
-
         <Input
           required
-          placeholder="Quantity"
-          type="number"
+          placeholder="Name"
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Country:
+        <Input
+          required
+          placeholder="Country"
+          value={country}
+          onChange={e => setCountry(e.target.value)}
+        />
+      </label>
+
+      <label>
+        Category:
+        <Input
+          required
+          placeholder="Category"
+          value={category}
+          onChange={e => setCategory(e.target.value)}
         />
       </label>
     </aside>
