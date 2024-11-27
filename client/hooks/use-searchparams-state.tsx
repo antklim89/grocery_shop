be42b148f417719896d41c6f4b02d1078c81cced
Anton <antklim89@mail.ru>
2024-11-27 19:32:53 +0300

    feat(hooks): create useSearchParamsState

---
+++ client/hooks/use-searchparams-state.tsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/navigation';
+import {
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+
+
+export function useSearchParamsState(key: string, initState: string = '') {
+  const [state, setState] = useState(() => new URLSearchParams(location.search).get(key) ?? initState);
+  const timeoutId = useRef<NodeJS.Timeout | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const newSearchParams = new URLSearchParams(location.search);
+    if (state.length === 0) newSearchParams.delete(key);
+    else newSearchParams.set(key, state);
+
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+    timeoutId.current = setTimeout(() => {
+      if (newSearchParams.toString() === new URLSearchParams(location.search).toString()) return;
+      router.replace(`?${newSearchParams.toString()}`);
+    }, 700);
+
+    return () => {
+      if (timeoutId.current) clearTimeout(timeoutId.current);
+    };
+  }, [state, router, key]);
+
+  return [state, setState] as const;
+}
