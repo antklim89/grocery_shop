b04d96594b46ec1525f086aa35c81193d2e8b654
Anton <=>
2024-12-03 22:55:01 +0300

    fix: add skipTotal

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,10 +8,10 @@ async function HomePage() {
       <Hero />
       <section className="container space-y-4 md:space-y-12 my-4 md:my-8">
         <h2 className="text-4xl font-bold mb-4 ml-8">New Products</h2>
-        <ProductList perPage={6} sort="-created,id" />
+        <ProductList skipTotal perPage={6} sort="-created,id" />
 
         <h2 className="text-4xl font-bold mb-4 ml-8">Best Discount</h2>
-        <ProductList perPage={6} sort="-discount" />
+        <ProductList skipTotal perPage={6} sort="-discount" />
       </section>
     </div>
   );
